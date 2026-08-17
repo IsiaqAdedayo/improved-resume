3ec7de91596a8b10171f0c5434658b97b1e2854d
isiaqAdedayo <hadedaryor@gmail.com>
2026-08-17 16:07:11 +0100

    feat: integrate Vercel Analytics into root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
+
 import { Cormorant_Garamond, Syne, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -37,6 +39,7 @@ export default function RootLayout({
         className={`${cormorantGaramond.variable} ${syne.variable} ${jetbrainsMono.variable} antialiased`}
       >
         {children}
+				<Analytics />
       </body>
     </html>
   );
